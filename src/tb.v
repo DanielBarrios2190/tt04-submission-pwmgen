6e5db0569006a578bb72a587a1b2ca0e0de426c5
Daniel Barrios <68023784+DanielBarrios2190@users.noreply.github.com>
2023-08-27 15:21:35 -0500

    Corrigiendo test

--- src/tb.v
+++ src/tb.v
@@ -1,62 +1,46 @@
-`timescale 1ns / 1ps
+`default_nettype none
+`timescale 1ns/1ps
 
-module tt_um_db_PWM(
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to 
-    
-    );
-    
-    wire [2:0] bits ;
+/*
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
 
-    reg [7:0] cnt;
-    wire [7:0] duty;
-    reg pwm_q,ppm_q;
-    reg [2:0] bits_pre;
-    wire pwm_d,ppm_d;
+// testbench is controlled by test.py
+module tb ();
 
-    assign duty = ui_in [7:0];
-    assign bits = uio_in[2:0];
-
-    assign uio_oe = 8'b11111000;
-    assign uio_out[7:0] = 8'b00000000;
-    assign uo_out[7:1] = 7'b0000000;
-    
-    always @(posedge clk) begin
-      if(rst_n) begin
-        bits_pre <= bits;
-        if(bits_pre != bits) begin
-            cnt <= 8'd0;
-            pwm_q <= 1'b0;
-        end
-        else begin
-            
-            pwm_q <= pwm_d;
-                     
-            if((cnt >= (2**bits)))
-                cnt <= 0;
-            else begin     
-                cnt <= cnt + 1;
-            end
-        end
-        bits_pre <= bits;
-      end else begin
-         pwm_q <= 1'b0;
-         cnt <= 0;
-         bits_pre <= bits;
-      end
+    // this part dumps the trace to a vcd file that can be viewed with GTKWave
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
     end
 
-    
-    assign pwm_d = (cnt < duty);
-    assign uo_out[0] = pwm_q;
-    
+    // wire up the inputs and outputs
+    reg  clk;
+    reg  rst_n;
+    reg  ena;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
+    tt_um_db_PWM tt_um_db_PWM (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
 
 endmodule
 
